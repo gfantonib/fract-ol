ed3bf99a6568c2950cecede024352ddc8623759f
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2023-11-29 18:59:10 -0300

    removing fucking commented function

--- bonus_sources/sierpinsky_bonus.c
+++ bonus_sources/sierpinsky_bonus.c
@@ -6,80 +6,11 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 14:28:15 by gfantoni          #+#    #+#             */
-/*   Updated: 2023/11/21 19:57:48 by gfantoni         ###   ########.fr       */
+/*   Updated: 2023/11/29 18:57:06 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/bonus_fractol.h"
-
-/* void	ft_bresenham_h(t_point a, int32_t dx, int32_t dy, t_fractal *fractal)
-{
-	int32_t	i;
-	int32_t	dis;
-
-	i = 0;
-	dis = (2 * abs(dy)) - abs(dx);
-	while (i++ < abs(dx))
-	{
-		if (dx < 0)
-			a.x = a.x - 1;
-		else
-			a.x = a.x + 1;
-		if (dis < 0)
-			dis = dis + 2 * abs(dy);
-		else
-		{
-			if (dy < 0)
-				a.y = a.y - 1;
-			else
-				a.y = a.y + 1;
-			dis = dis + 2 * (abs(dy) - abs(dx));
-		}
-		if (!ft_point_validation(a))
-			mlx_put_pixel(fractal->canvas, a.x, a.y, 0xFFFFFFF);
-	}
-}
-
-void	ft_bresenham_v(t_point a, int32_t dx, int32_t dy, t_fractal *fractal)
-{
-	int32_t	dis;
-	int32_t	i;
-
-	i = 0;
-	dis = (2 * abs(dx)) - abs(dy);
-	while (i++ < abs(dy))
-	{
-		if (dy < 0)
-			a.y = a.y - 1;
-		else
-			a.y = a.y + 1;
-		if (dis < 0)
-			dis = dis + 2 * abs(dx);
-		else
-		{
-			if (dx < 0)
-				a.x = a.x - 1;
-			else 
-				a.x = a.x + 1;
-			dis = dis + 2 * (abs(dx) - abs(dy));
-		}
-		if (!ft_point_validation(a))
-			mlx_put_pixel(fractal->canvas, a.x, a.y, 0xFFFFFFFF);
-	}
-}
-
-void	ft_bresenham(t_point a, t_point b, t_fractal *fractal)
-{
-	int32_t	dx;
-	int32_t	dy;
-
-	dx = b.x - a.x;
-	dy = b.y - a.y;
-	if (abs(dx) > abs(dy))
-		ft_bresenham_h(a, dx, dy, fractal);
-	else
-		ft_bresenham_v(a, dx, dy, fractal);
-} */
 
 t_point	ft_middle(t_point a, t_point b)
 {
